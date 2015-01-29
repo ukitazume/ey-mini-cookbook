ee03f0ba9b66483cc34d7acfbeeae8cd3e25b477
Yu Kitazume <u.kitazume@gmail.com>
2015-01-29 15:01:47 +0900

    fluentd

--- cookbooks/fluentd/recipes/default.rb
+++ cookbooks/fluentd/recipes/default.rb
@@ -3,6 +3,10 @@ execute "gem install fluentd" do
   user "root"
 end
 
+execute "gem install bundler" do 
+  command "gem install bundler"
+  user "root"
+end
 
 template "/etc/init.d/fluentd" do
   owner 'root'
@@ -18,6 +22,9 @@ directory '/etc/fluentd/' do
   group  'deploy'
   mode   0755
   action :create
+  notifies :run, "directory[/etc/fluentd/plugin]"
+  notifies :run, "template[/etc/fluentd/fluent.conf]"
+  notifies :run, "template[/etc/fluentd/Gemfile]"
 end
 
 template "/etc/fluentd/fluent.conf" do
@@ -44,4 +51,48 @@ file '/var/log/fluentd.log' do
   mode   0660
   action :create
   not_if { File.exists?("/var/log/fluentd.log") }
+end
+
+directory '/etc/fluentd/plugin' do
+  owner  'deploy'
+  group  'deploy'
+  mode   0755
+  action :create
+  only_if { File.exists?("/etc/fluentd") }
+  notifies :run, "execute[plugin install]"
+end
+
+template "/etc/fluentd/Gemfile" do
+  owner 'deploy'
+  group 'deploy'
+  mode 0755
+  source "Gemfile"
+  variables({
+  })
+  only_if { File.exists?("/etc/fluentd") }
+end
+
+execute "plugin install" do
+  command "bundle install --path /etc/fluentd/plugin"
+  cwd "/etc/fluentd"
+  user "deploy"
+  only_if { File.exists?("/etc/fluentd/plugin") }
+end
+
+template "/data/monit.d/fluentd.monitrc" do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source "fluentd.monitrc.erb"
+  notifies :run, "execute[monit reload]"
+end
+
+execute "monit reload" do
+  command "monit reload"
+  action :run
+end
+
+execute "fluentd reload" do
+  command "monit restart fluentd"
+  user "root"
 end
