ddaaceb8b1475c0a353ebb7e357ae0209e3efa09
Yu Kitazume <u.kitazume@gmail.com>
2015-07-31 10:59:26 +0900

    add init file and run level

--- cookbooks/pm2/recipes/default.rb
+++ cookbooks/pm2/recipes/default.rb
@@ -1,8 +1,16 @@
 execute "install pm2" do
   command "npm install pm2 -g"
+  notifies :run, "execute[link pm2]"
 end
 
 execute "link pm2" do
   command "ln -s /opt/nodejs/current/bin/pm2  /usr/bin/pm2"
   not_if { File.exists?("/usr/bin/pm2") }
+  notifies :run, "execute[init script and rc level]"
+end
+
+# TODO: https://github.com/Unitech/PM2/issues/1063
+execute 'init script and rc level' do
+  command "pm2 startup gentoo || mv /etc/init.d/pm2-init.sh  /etc/init.d/pm2 && chmod +x /etc/init.d/pm2 && rc-update add pm2 default"
+  only_if { ::File.exists?("/usr/bin/pm2") }
 end
