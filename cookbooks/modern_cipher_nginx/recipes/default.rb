95f5346e44b2d8f86ef3560ec96e878e2baf72e5
Yu Kitazume <u.kitazume@gmail.com>
2015-09-17 10:02:38 +0900

    modern cipher suits for nginx

---
+++ cookbooks/modern_cipher_nginx/recipes/default.rb
@@ -0,0 +1,18 @@
+node[:applications].each do |app_name, _|
+  if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+    template "/etc/nginx/servers/#{app_name}/customer.ssl_cipher" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      backup false
+      mode 0777
+      source "customer.ssl_cipher.erb"
+      variables({
+        :app_name => app_name
+      })
+    end
+  end
+end
+
+service "nginx" do
+  action :reload
+end
